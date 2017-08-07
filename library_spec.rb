88ddcb84081d1cc6fe60cf585449d8f6f7aebd87
Heather Rae <heatherrae150@yahoo.co.uk>
2017-08-07 22:54:07 +0100

    Add instructions to spec file

--- library_spec.rb
+++ library_spec.rb
@@ -107,7 +107,25 @@ def test_book_info
 end
 
 
+# Create a method that takes in a book title
+# and returns only the rental details for
+# that book.
 
+
+
+
+# Create a method that takes in a book title
+# and adds it to our book list (Add a new
+# hash for the book with the student name
+# and date being left as empty strings)
+
+
+
+
+# Create a method that changes the rental
+# details of a book by taking in the title
+# of the book, the student renting it and
+# the date it's due to be returned.
 
 
 
